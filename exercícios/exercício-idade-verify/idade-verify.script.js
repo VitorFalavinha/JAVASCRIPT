314066451dfaea0e252b059f71f149dfbe380a50
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-07-21 19:29:49 -0300

    finish exercise idade verify

--- exercícios/exercício-idade-verify/idade-verify.script.js
+++ exercícios/exercício-idade-verify/idade-verify.script.js
@@ -11,12 +11,40 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
     if (fsex[0].checked){
         genero = 'Homem'
+        if (idade >=0 && idade < 10){
+            img.setAttribute('src', 'imagens/criança.menino.jpg')
+            
+        } else if (idade < 21){
+            img.setAttribute('src', 'imagens/adolescente.menino.jpg')
+        } else if (idade < 50){
+            img.setAttribute('src', 'imagens/adulto.homem.jpg')
+        } else {
+            img.setAttribute('src', 'imagens/idoso.homem.jpg')
+        }
+
+
     }else if (fsex[1].checked){
         genero = 'Mulher'
+        if (idade >=0 && idade < 10){
+            img.setAttribute('src', 'imagens/criança.menina.jpg')
+        } else if (idade < 21){
+            img.setAttribute('src', 'imagens/adolescente.menina.jpg')
+        } else if (idade < 50){
+            img.setAttribute('src', 'imagens/adulto.mulher.jpg')
+        } else {
+            img.setAttribute('src', 'imagens/idoso.mulher.jpg')
+        }
     }
+    res.style.textAlign = 'center'
+    img.style.borderRadius = '25px'
+    img.style.marginTop = '15px'
+    img.style.boxShadow = '2px 2px 10px white'
     res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
 }
 }
